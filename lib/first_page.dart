f5d90b56bf2a36c152a6bc34b25da399e2993617
syadool <takerumorimoto0417@gmail.com>
2022-12-15 21:13:30 +0900

    値渡しの実装

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:widjet/second_page.dart';
 
 class FirstPage extends StatelessWidget{
+  String nametext = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,15 +11,30 @@ class FirstPage extends StatelessWidget{
         title: const Text('画面遷移する人。'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            //ボタンが押されたときに呼ばれるコード
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>  SecondPage()),
-            );
-          },
-          child: const Text('次の画面へ'),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                onChanged: (text){
+                  nametext = text;
+                },
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  //ボタンが押されたときに呼ばれるコード
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>  SecondPage(nametext),
+                    ),
+                  );
+                },
+                child: const Text('次の画面へ'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget{
+  const SecondPage(this.nametext);
+  final String nametext;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
         title: const Text('2番目'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            //ボタンが押されたときに呼ばれるコード
-
-          },
-          child: const Text('前の画面へ'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              nametext,
+              style: const TextStyle(
+                fontSize: 50
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                //ボタンが押されたときに呼ばれるコード
+                Navigator.pop(context);
+              },
+              child: const Text('前の画面へ'),
+            ),
+          ],
         ),
       ),
     );
