9087cb8b3c97a64f4d943a9fa9c682eb046da267
syadool <takerumorimoto0417@gmail.com>
2022-12-20 08:57:35 +0900

    概要の文字実装

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -7,29 +7,63 @@ class FirstPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    final List<String> entries = <String>['うんこ系YouTuber、ヒカキン', 'あんこ型YouTuber、デカキン', '擬音系YouTuber、セイキン'];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('画面遷移する人。'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          Container(
-            height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('Entry A')),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[500],
-            child: const Center(child: Text('Entry B')),
-          ),
-          Container(
-            height: 50,
-            color: Colors.amber[100],
-            child: const Center(child: Text('Entry C')),
-          ),
+        title: const Text('なっつー'),
+        backgroundColor: Colors.black87,
+        actions: [
+          Icon(Icons.cast),
+          SizedBox(width: 24,),
+          Icon(Icons.search),
+          SizedBox(width: 24,),
+          Icon(Icons.menu),
+          SizedBox(width: 24,),
         ],
+      ),
+      body: Container(
+        color: Colors.black87,
+        child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 100,
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Image.network('https://i.ytimg.com/vi/QdcMb97UthI/maxresdefault.jpg',
+                    height: 100,
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            entries[index],
+                            style: TextStyle(
+                            color: Colors.white,
+                              fontSize: 17
+                            ),
+                          ),
+                          Text(
+                            '10回・５分前',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12.5
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+        )
       )
     );
   }
